514fc560c9b0456d6f12787a12ccdacd4675c6ba
gelitshh-hue <gelitshh@gmail.com>
2026-01-22 22:27:29 +0200

    -

---
+++ resources/views/admin/include/script.blade.php
@@ -0,0 +1,17 @@
+ <script src="{{asset('bashdoard/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{asset('bashdoard/assets/vendors/chart.js/chart.umd.js')}}"></script>
+    <script src="{{asset('bashdoard/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{asset('bashdoard/assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('bashdoard/assets/js/misc.js')}}"></script>
+    <script src="{{asset('bashdoard/assets/js/settings.js')}}"></script>
+    <script src="{{asset('bashdoard/assets/js/todolist.js')}}"></script>
+    <script src="{{asset('bashdoard/assets/js/jquery.cookie.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="{{asset('bashdoard/assets/js/dashboard.js')}}"></script>
+    <!-- End custom js for this page -->
+  </body>
